de01bc5c13470fff0a665734c9d3a3637b9f010e
Talha-dev1980 <dotdev1980@gmail.com>
2025-05-31 20:34:52 +0200

    First Push

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDvWcQ8h8tBLVkWSagaAgCIvA3AIn0H5JE",
+    authDomain: "viennaguide-7bd20.firebaseapp.com",
+    databaseURL: "https://viennaguide-7bd20-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "viennaguide-7bd20",
+    storageBucket: "viennaguide-7bd20.firebasestorage.app",
+    messagingSenderId: "136111749955",
+    appId: "1:136111749955:web:f5841fbeca32fb8bb90df6",
+    measurementId: "G-7LC5S4Z82T"
+  };
